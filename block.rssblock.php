b11b7c7727f351c8b864f551f00b783da7340ee3
ringmaster <epithet@gmail.com>
2013-01-21 17:46:02 -0500

    Remove extra block template parts.

--- block.rssblock.php
+++ block.rssblock.php
@@ -1,12 +1,9 @@
-<div class="rssblock" id="<?php echo $content->markup_id; ?>">
-	<h3><?php echo $content->title; ?></h3>
-	<ul>
-	<?php $items = $content->items; foreach($items as $item): ?>
-		<li><a href="<?php echo $item->link; ?>">
-		<?php if(isset($item->media__thumbnail__url)): ?>
-		<img src="<?php echo $item->media__thumbnail__url; ?>" class="media thumbnail">
-		<?php endif; ?>
-		<?php echo $item->title; ?></a></li>
-	<?php endforeach; ?>
-	</ul>
-</div>
+<ul>
+<?php $items = $content->items; foreach($items as $item): ?>
+	<li><a href="<?php echo $item->link; ?>">
+	<?php if(isset($item->media__thumbnail__url)): ?>
+	<img src="<?php echo $item->media__thumbnail__url; ?>" class="media thumbnail">
+	<?php endif; ?>
+	<?php echo $item->title; ?></a></li>
+<?php endforeach; ?>
+</ul>
